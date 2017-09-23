c994a215159458c5d3dbcaee971ceaa1f7a75633
Peter Grauvogel <peter@grauvoegel.de>
2017-09-23 22:58:12 +0200

    Load resources from the main hamilton theme.

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+function my_theme_enqueue_styles() {
+
+    $parent_style = 'hamilton'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+
+	wp_enqueue_style( 'hamilton_fontawesome', get_stylesheet_directory_uri() . '/fa/css/font-awesome.css' );
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles');
+?>
